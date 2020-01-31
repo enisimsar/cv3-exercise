0baca065b0b58ed4aded37df56b715d6f3745081
Enis Simsar <enisimsar@gmail.com>
2020-01-31 17:21:46 +0300

    Close to SOTA

--- src/tracker/object_detector.py
+++ src/tracker/object_detector.py
@@ -1,7 +1,9 @@
 import torch
 import torch.nn.functional as F
 
+from collections import OrderedDict
 from torchvision.models.detection import FasterRCNN
+from torchvision.models.detection.transform import resize_boxes
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 
@@ -19,4 +21,147 @@ class FRCNN_FPN(FasterRCNN):
 
         detections = self(img)[0]
 
+        return detections['boxes'].detach(), detections['scores'].detach()
         return detections['boxes'].detach().cpu(), detections['scores'].detach().cpu()
+
+    def detect_with_proposal(self, img, t_1_proposal):
+        """
+            https://github.com/pytorch/vision/blob/master/torchvision/models/detection/generalized_rcnn.py
+        """
+        images = img
+            
+        device = list(self.parameters())[0].device
+        images = images.to(device)
+        
+        original_image_sizes = []
+        for img in images:
+            val = img.shape[-2:]
+            assert len(val) == 2
+            original_image_sizes.append((val[0], val[1]))
+
+        images, _ = self.transform(images, None)
+        features = self.backbone(images.tensors)
+        if isinstance(features, torch.Tensor):
+            features = OrderedDict([('0', features)])
+
+        if not len(t_1_proposal):
+            return torch.Tensor([]), torch.Tensor([])
+
+        tt = resize_boxes(
+                t_1_proposal.to(device),
+                original_image_sizes[0], images.image_sizes[0]
+        )
+
+        # detections, _ = self.roi_heads(features, tt, images.image_sizes, None)
+        # detections = self.transform.postprocess(detections, images.image_sizes, original_image_sizes)
+
+        # detections = detections[0]
+
+        box_features = self.roi_heads.box_roi_pool(
+            features, [tt], images.image_sizes)
+        box_features = self.roi_heads.box_head(box_features)
+        class_logits, box_regression = self.roi_heads.box_predictor(
+            box_features)
+
+        pred_boxes = self.roi_heads.box_coder.decode(box_regression, [tt])
+        pred_scores = F.softmax(class_logits, -1)
+
+        pred_boxes = pred_boxes[:, 1:].squeeze(dim=1).detach()
+        pred_boxes = resize_boxes(
+            pred_boxes, images.image_sizes[0], original_image_sizes[0])
+        pred_scores = pred_scores[:, 1:].squeeze(dim=1).detach()
+
+        # for bbox in pred_boxes:
+        #     bbox[0] = max(bbox[0], 0)
+        #     bbox[1] = max(bbox[1], 0)
+        #     bbox[2] = min(bbox[2], original_image_sizes[0][0])
+        #     bbox[3] = min(bbox[3], original_image_sizes[0][0])
+
+        return pred_boxes, pred_scores
+
+    def detect_with_proposals(self, img, t_1_proposals):
+        """
+            https://github.com/pytorch/vision/blob/master/torchvision/models/detection/generalized_rcnn.py
+        """     
+        with torch.no_grad():       
+            images = img
+            device = list(self.parameters())[0].device
+            images = images.to(device)
+
+            original_image_sizes = []
+            for img in images:
+                val = img.shape[-2:]
+                assert len(val) == 2
+                original_image_sizes.append((val[0], val[1]))
+
+            images, _ = self.transform(images, None)
+            features = self.backbone(torch.cat([images.tensors for _ in range(len(t_1_proposals))], dim=0))
+            
+            tt = []
+            for t in t_1_proposals:
+                tt.append(
+                    resize_boxes(
+                        torch.Tensor(t).unsqueeze(0).to(device),
+                        original_image_sizes[0], images.image_sizes[0]
+                    )
+                )
+
+            detections, _ = self.roi_heads(features, tt, images.image_sizes * len(tt), None)
+            detections = self.transform.postprocess(detections, images.image_sizes, original_image_sizes)
+
+            return [(detection['boxes'].detach().cpu(), detection['scores'].detach().cpu()) for detection in detections]
+
+    def predict_boxes(self, images, boxes):
+        device = list(self.parameters())[0].device
+        images = images.to(device)
+        boxes = boxes.to(device)
+
+        targets = None
+        original_image_sizes = [img.shape[-2:] for img in images]
+
+        images, targets = self.transform(images, targets)
+
+        features = self.backbone(images.tensors)
+        if isinstance(features, torch.Tensor):
+            features = OrderedDict([(0, features)])
+
+        # proposals, proposal_losses = self.rpn(images, features, targets)
+        from torchvision.models.detection.transform import resize_boxes
+        boxes = resize_boxes(
+            boxes, original_image_sizes[0], images.image_sizes[0])
+        proposals = [boxes]
+
+        box_features = self.roi_heads.box_roi_pool(
+            features, proposals, images.image_sizes)
+        box_features = self.roi_heads.box_head(box_features)
+        class_logits, box_regression = self.roi_heads.box_predictor(
+            box_features)
+
+        pred_boxes = self.roi_heads.box_coder.decode(box_regression, proposals)
+        pred_scores = F.softmax(class_logits, -1)
+
+
+        # score_thresh = self.roi_heads.score_thresh
+        # nms_thresh = self.roi_heads.nms_thresh
+
+        # self.roi_heads.score_thresh = self.roi_heads.nms_thresh = 1.0
+        # self.roi_heads.score_thresh = 0.0
+        # self.roi_heads.nms_thresh = 1.0
+        # detections, detector_losses = self.roi_heads(
+        #     features, [boxes.squeeze(dim=0)], images.image_sizes, targets)
+
+        # self.roi_heads.score_thresh = score_thresh
+        # self.roi_heads.nms_thresh = nms_thresh
+
+        # detections = self.transform.postprocess(
+        #     detections, images.image_sizes, original_image_sizes)
+
+        # detections = detections[0]
+        # return detections['boxes'].detach().cpu(), detections['scores'].detach().cpu()
+
+        pred_boxes = pred_boxes[:, 1:].squeeze(dim=1).detach()
+        pred_boxes = resize_boxes(
+            pred_boxes, images.image_sizes[0], original_image_sizes[0])
+        pred_scores = pred_scores[:, 1:].squeeze(dim=1).detach()
+        return pred_boxes, pred_scores
+        
--- src/tracker/tracker.py
+++ src/tracker/tracker.py
@@ -1,9 +1,33 @@
-import numpy as np
 import torch
 import torch.nn.functional as F
 
+import numpy as np
 import motmetrics as mm
 from torchvision.ops.boxes import clip_boxes_to_image, nms
+
+def bb_intersection_over_union(boxA, boxB):
+	# Source: https://www.pyimagesearch.com/2016/11/07/intersection-over-union-iou-for-object-detection/
+	# determine the (x, y)-coordinates of the intersection rectangle
+	xA = max(boxA[0], boxB[0])
+	yA = max(boxA[1], boxB[1])
+	xB = min(boxA[2], boxB[2])
+	yB = min(boxA[3], boxB[3])
+ 
+	# compute the area of intersection rectangle
+	interArea = max(0, xB - xA + 1) * max(0, yB - yA + 1)
+ 
+	# compute the area of both the prediction and ground-truth
+	# rectangles
+	boxAArea = (boxA[2] - boxA[0] + 1) * (boxA[3] - boxA[1] + 1)
+	boxBArea = (boxB[2] - boxB[0] + 1) * (boxB[3] - boxB[1] + 1)
+ 
+	# compute the intersection over union by taking the intersection
+	# area and dividing it by the sum of prediction + ground-truth
+	# areas - the interesection area
+	iou = (interArea + 1e-8) / float(boxAArea + boxBArea - interArea + 1e-8)
+ 
+	# return the intersection over union value
+	return iou
 
 
 class Tracker:
@@ -13,6 +37,8 @@ class Tracker:
 		self.obj_detect = obj_detect
 
 		self.tracks = []
+		self.track_actives = []
+
 		self.track_num = 0
 		self.im_index = 0
 		self.results = {}
@@ -20,6 +46,8 @@ class Tracker:
 		self.mot_accum = None
 
 	def reset(self, hard=True):
+		print('res')
+		self.track_actives = []
 		self.tracks = []
 
 		if hard:
@@ -31,19 +59,20 @@ class Tracker:
 		"""Initializes new Track objects and saves them."""
 		num_new = len(new_boxes)
 		for i in range(num_new):
-			self.tracks.append(Track(
-				new_boxes[i],
-				new_scores[i],
+			self.track_actives.append(Track(
+			# self.tracks.append(Track(
+				new_boxes[i].cpu().numpy(),
+				new_scores[i].cpu().numpy(),
 				self.track_num + i
 			))
 		self.track_num += num_new
 
 	def get_pos(self):
 		"""Get the positions of all active tracks."""
-		if len(self.tracks) == 1:
-			box = self.tracks[0].box
-		elif len(self.tracks) > 1:
-			box = torch.stack([t.box for t in self.tracks], 0)
+		if len(self.track_actives) == 1:
+			box = torch.Tensor(self.track_actives[0].box)
+		elif len(self.track_actives) > 1:
+			box = torch.stack([torch.Tensor(t.box) for t in self.track_actives], 0)
 		else:
 			box = torch.zeros(0).cuda()
 		return box
@@ -51,21 +80,87 @@ class Tracker:
 	def data_association(self, boxes, scores):
 		self.tracks = []
 		self.add(boxes, scores)
-
+		
 	def step(self, frame):
 		"""This function should be called every timestep to perform tracking with a blob
 		containing the image information.
 		"""
-		# object detection
-		boxes, scores = self.obj_detect.detect(frame['img'])
+		# batch_size = 1
+		B = []
+		S = []
+		# self.tracks = []
 
-		self.data_association(boxes, scores)
+		# bt_1s = []
+		# for i, t_active in enumerate(self.track_actives):
+		# 	bt_1s.append(t_active.box)
+
+		# bt_1s = np.array(bt_1s)
+		# results = []
+		# for batch_i in range(0, len(bt_1s), batch_size):
+		# 	batch = bt_1s[batch_i: batch_i + batch_size]
+		# 	for res in self.obj_detect.detect_with_proposals(frame['img'], batch):
+		# 		results.append(res)
+
+		results = []
+		boxes, scores = self.obj_detect.detect_with_proposal(frame['img'], self.get_pos())
+		boxes = clip_boxes_to_image(boxes, frame['img'].shape[-2:])
+
+		results = list(zip(boxes.cpu().numpy(), scores.cpu().numpy()))
+
+		removed_actives = []
+		for i, t_active in enumerate(self.track_actives):
+			b_t, s_t = results[i]
+
+			if s_t > 0.5:
+				B.append(b_t)
+				S.append(s_t)
+				continue
+
+			removed_actives.append(i)
+		self.track_actives = [t for i, t in enumerate(self.track_actives) if i not in removed_actives]
+
+		B = np.array(B)
+		S = np.array(S)
+		kept_indices = nms(torch.Tensor(B), torch.Tensor(S), 0.6) # lambda_active
+
+		self.track_actives = list(np.array(self.track_actives)[kept_indices])
+
+		B = np.array(B)[kept_indices] if len(kept_indices) else B
+
+		for t_active, b_t in zip(self.track_actives, B):
+			t_active.prev_boxes.append(b_t)
+			t_active.box = b_t
+
+		boxes, scores = self.obj_detect.detect(frame['img'])
+		removed_boxes = []
+		for i, box in enumerate(boxes):
+			if scores[i] < 0.5: 
+				removed_boxes.append(i)
+				continue
+			for b_t in B:
+				iou_score = bb_intersection_over_union(box, b_t)
+				if iou_score > 0.4: # lambda_new
+					removed_boxes.append(i)
+		boxes = [b for i, b in enumerate(boxes) if i not in removed_boxes]
+		scores = [s for i, s in enumerate(scores) if i not in removed_boxes]
+
+		self.add(boxes, scores)
+		
+		# object detection
+		# boxes, scores = self.obj_detect.detect(frame['img'])
+
+		# self.data_association(boxes, scores)
 
 		# results
-		for t in self.tracks:
+		# for t in self.tracks:
+		# 	if t.id not in self.results.keys():
+		# 		self.results[t.id] = {}
+		# 	self.results[t.id][self.im_index] = np.concatenate([t.box, np.array([t.score])])
+
+		for t in self.track_actives:
 			if t.id not in self.results.keys():
 				self.results[t.id] = {}
-			self.results[t.id][self.im_index] = np.concatenate([t.box.cpu().numpy(), np.array([t.score])])
+			self.results[t.id][self.im_index] = np.concatenate([t.box, np.array([t.score])])
 
 		self.im_index += 1
 
@@ -79,4 +174,5 @@ class Track(object):
 	def __init__(self, box, score, track_id):
 		self.id = track_id
 		self.box = box
+		self.prev_boxes = [box]
 		self.score = score
